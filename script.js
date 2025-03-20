0d5a4963347244c4f6308f1c49729724b75651ff
Shreyas Joshi <G00440608@atu.ie>
2025-03-20 17:13:31 +0000

    Updated script.js to update the image when clicked

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let subheading = document.getElementById("subheading");
+let timmyPic = document.getElementById("timmy");
 
 let openRelatedVideo = () => {
     window.location.replace("https://youtu.be/dQw4w9WgXcQ?feature=shared");
 }
 
+let changeImage = () => {
+    timmyPic.src = "img/rick.png";
+    subheading.innerHTML = "do NOT click on this";
+}
+
 subheading.addEventListener('click', openRelatedVideo);
+timmyPic.addEventListener('click',changeImage);
