cf5c2dbbb30128641f026ef260d2ae5e06388479
Shreyas Joshi <G00440608@atu.ie>
2025-03-20 16:50:31 +0000

    Updated script.js (attempt to fix eventListener)

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let subheading = document.getElementById("subheading");
 
-subheading.addEventListener('click', window.location.replace("https://https://youtu.be/dQw4w9WgXcQ?feature=shared"));
+subheading.addEventListener('click', window.location.replace("https://youtu.be/dQw4w9WgXcQ?feature=shared"));
